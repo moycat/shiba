efec0a4fd6fe6d0990a66111d7e733899c29cf7d
Moycat <i@moy.cat>
2022-01-27 02:54:41 +0800

    fix exceeding nic name length limit

--- util/uid.go
+++ util/uid.go
@@ -8,11 +8,11 @@ import (
 	"time"
 )
 
-var counter uint16
+var counter uint8
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-	counter = uint16(rand.Int() % 65536)
+	counter = uint8(rand.Int() % 255)
 }
 
 // NewUID tries to return a locally unique ID.
@@ -21,6 +21,6 @@ func NewUID() string {
 	b := make([]byte, 4)
 	binary.LittleEndian.PutUint32(b, uint32(time.Now().Unix()))
 	counter++
-	b = append(b, byte(counter>>8), byte(counter))
-	return strings.ToLower(base32.StdEncoding.EncodeToString(b))[:10]
+	b = append(b, counter)
+	return strings.ToLower(base32.StdEncoding.EncodeToString(b))
 }
